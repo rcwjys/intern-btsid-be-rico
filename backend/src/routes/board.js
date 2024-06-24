1480424fccdefe0d296b8c571531625222fedb79
rcwjys <rcwjys@gmail.com>
2024-06-24 18:11:27 +0700

    <fixing> memperbaiki dokumentasi untuk endpoint mengambil data

--- backend/src/routes/board.js
+++ backend/src/routes/board.js
@@ -33,49 +33,74 @@ const boardRouter = express.Router();
  *                         properties:
  *                           board_id:
  *                             type: string
- *                             example: 0d08b4db-9775-4a43-ab63-a6ce552d29be
+ *                             example: c4c9a5d7-42b0-47f0-8b99-9d9a3ee91b69
  *                           board_title:
  *                             type: string
  *                             example: my first board
- *                           createdAt:
- *                             type: string
- *                             format: date-time
- *                             example: "2024-06-20T04:49:00.567Z"
- *                           updatedAt:
- *                             type: string
- *                             format: date-time
- *                             example: "2024-06-20T04:49:00.567Z"
- *                           author:
- *                             type: object
- *                             properties:
- *                               user_id:
- *                                 type: string
- *                                 example: f9448035-2e75-42b6-851b-7a3016835c5e
- *                               user_name:
- *                                 type: string
- *                                 example: rico wijaya
- *       '404':
- *         description: Board not found error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                 error:
- *                   type: object
- *                   properties:
- *                     message:
- *                       type: string
- *                       example: board is not found
- * securitySchemes:
- *   BearerAuth:
- *     type: http
- *     scheme: bearer
- *     bearerFormat: JWT
+ *                           lists:
+ *                             type: array
+ *                             items:
+ *                               type: object
+ *                               properties:
+ *                                 list_id:
+ *                                   type: string
+ *                                   example: 0a463c71-15f3-4647-a7b8-794438b6345f
+ *                                 list_title:
+ *                                   type: string
+ *                                   example: todo
+ *                                 createdAt:
+ *                                   type: string
+ *                                   format: date-time
+ *                                   example: "2024-06-24T08:12:09.757Z"
+ *                                 updatedAt:
+ *                                   type: string
+ *                                   format: date-time
+ *                                   example: "2024-06-24T08:12:09.757Z"
+ *                                 tasks:
+ *                                   type: array
+ *                                   items:
+ *                                     type: object
+ *                                     properties:
+ *                                       task_id:
+ *                                         type: string
+ *                                         example: 55ff014c-3086-4c7d-afa5-65983436b5c1
+ *                                       task_title:
+ *                                         type: string
+ *                                         example: nyapu
+ *                                       createdAt:
+ *                                         type: string
+ *                                         format: date-time
+ *                                         example: "2024-06-24T11:04:05.175Z"
+ *                                       updatedAt:
+ *                                         type: string
+ *                                         format: date-time
+ *                                         example: "2024-06-24T11:04:05.175Z"
+ *           example:
+ *             success: true
+ *             data:
+ *               boards:
+ *                 - board_id: "c4c9a5d7-42b0-47f0-8b99-9d9a3ee91b69"
+ *                   board_title: "my first board"
+ *                   lists:
+ *                     - list_id: "0a463c71-15f3-4647-a7b8-794438b6345f"
+ *                       list_title: "todo"
+ *                       createdAt: "2024-06-24T08:12:09.757Z"
+ *                       updatedAt: "2024-06-24T08:12:09.757Z"
+ *                       tasks:
+ *                         - task_id: "55ff014c-3086-4c7d-afa5-65983436b5c1"
+ *                           task_title: "nyapu"
+ *                           createdAt: "2024-06-24T11:04:05.175Z"
+ *                           updatedAt: "2024-06-24T11:04:05.175Z"
+ *                         - task_id: "6b695561-6e49-4382-a1b7-271913a0d109"
+ *                           task_title: "makan"
+ *                           createdAt: "2024-06-24T08:29:03.238Z"
+ *                           updatedAt: "2024-06-24T08:29:03.238Z"
+ *                         - task_id: "df909913-b902-4cfc-a349-c9b9b8e95b99"
+ *                           task_title: "mandi"
+ *                           createdAt: "2024-06-24T08:28:58.067Z"
+ *                           updatedAt: "2024-06-24T08:28:58.067Z"
  */
+
 
 boardRouter.get("/api/v1/boards", tryCatch(getBoardData));
 /**
