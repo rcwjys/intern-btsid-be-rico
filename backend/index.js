978eb8016b5774bfb013054de55997d223de255a
rcwjys <rcwjys@gmail.com>
2024-07-12 11:11:36 +0700

    <fixing> Refactoring code in entrypoint

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,5 @@
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerOptions from './src/swagger/swaggerOptions.js';
+import {app} from "./src/application/web.js";
 
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { authRouter } from './src/routes/auth.js';
-import { errorHandling } from './src/utils/errorHandling.js';
-import { boardRouter } from './src/routes/board.js';
-import { listRouter } from './src/routes/list.js';
-import { taskRouter } from './src/routes/task.js';
-import { authenticateMiddleware } from './src/middleware/authenticate.js';
-import { tryCatch } from './src/utils/tryCatch.js';
-
-dotenv.config();
-
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.use(authRouter);
-
-app.use(tryCatch(authenticateMiddleware));
-
-app.use(boardRouter);
-
-app.use(listRouter);
-
-app.use(taskRouter);
-
-app.use(errorHandling);
 
 app.listen(process.env.PORT, () => {
   console.info(`Listening on port ${process.env.PORT}`);
